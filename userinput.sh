604c1e27b11636c01edd7cd71e810e2588d4bae3
vanshika1009 <46700983+vanshika1009@users.noreply.github.com>
2019-09-30 22:45:54 +0530

    simple userinput

---
+++ userinput.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+echo "Who are you?"
+read name
+echo "Welcome $name."  #simple usage od variable 
+
+echo "what are you doing?"
+read doe
+echo "So $name, you are ${doe}ing" #brackets are used when you've to append the variable 
